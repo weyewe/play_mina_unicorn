a597a4c26d083b8f2396c850a579d3cc9c93cd09
willy <w.yunnal@gmail.com>
2015-05-21 20:03:47 +0700

    added queue launch

--- config/deploy.rb
+++ config/deploy.rb
@@ -70,6 +70,10 @@ task :deploy => :environment do
     invoke :'rails:assets_precompile'
     invoke :'deploy:cleanup'
 
+    to :launch do
+      queue "restart shopper"
+    end
+
     # to :launch do
     #   queue "mkdir -p #{deploy_to}/#{current_path}/tmp/"
     #   queue "touch #{deploy_to}/#{current_path}/tmp/restart.txt"
